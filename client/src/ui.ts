4efcab350d5fd15d2bc1de196911ee270de7b191
orianodev <nono7g@proton.me>
2025-03-02 11:00:00 +0100

    refactor: prepare new modularized client structure

---
+++ client/src/ui.ts
@@ -0,0 +1,6 @@
+export function setLoadingScreen() { }
+export function setStadium() { }
+export function preloadImages() { }
+export function updateLateralColumns() { }
+export function displayPopup() { }
+export function showGameScreen() { }
